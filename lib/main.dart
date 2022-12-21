2ed014005cef5d34ee1812ceb90975514efd9163
Edisson David Prieto Vargas <v-edivargas@expediagroup.com>
2022-12-20 22:01:29 -0500

    Travel app
    - Second Challenge -> creation of a landing page using basic Widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -11,21 +11,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+
+          primarySwatch: Colors.grey,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                  ),
+                  Text("     Derrière"),
+                ],
+              ),
+              bottomOpacity: 100,
+            ),
+            body: Stack(
+              fit: StackFit.expand,
+              children: [
+                FittedBox(
+                  child: Image.asset('assets/images/Eiffel_tower.jpg'),
+                  fit: BoxFit.fill,
+                ),
+                Center(
+                  child: Container(
+                    height: 100,
+                    color: Color.fromRGBO(0, 0, 0, 0.5),
+                    child: Center(
+                      child: Text('Paris',
+                          style: TextStyle(fontSize: 40, color: Colors.white)),
+                    ),
+                  ),
+                )
+              ],
+            )
+            // <-- SEE HERE
+            ));
   }
 }
 
